93a70de6f17558552618ca38e5cb638e481d43de
Gabriel Colares <gabrielcdelgado37@gmail.com>
2024-11-21 00:59:08 -0300

    slides

---
+++ main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper', {
+    mousewheel: true,
+    lazy: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+  })
+  
+  document.querySelectorAll('.custom-next-button').forEach((button) => {
+    button.addEventListener('click', () => {
+      swiper.slideNext()
+    })
+  })
